c0784e9236ccfb123e13d181cf3487f62d1308ab
p4irin <p4irin.github.io@gmail.com>
2024-05-04 04:41:06 +0200

    Bump version 1.5.0 -> 1.6.0

--- src/c4t/__init__.py
+++ src/c4t/__init__.py
@@ -37,7 +37,7 @@ usage:
 
 __author__ = 'p4irin'
 __email__ = '139928764+p4irin@users.noreply.github.com'
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 
 
 import requests
--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ import c4t
 
 __author__ = 'p4irin'
 __email__ = '139928764+p4irin@users.noreply.github.com'
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 
 
 class C4tTests(unittest.TestCase):
